52008bc4ba20cf310b5fad09ae5f2417c14414e4
Foysal Ahmmed <afaysal410@gmail.com>
2020-12-08 20:36:04 +0600

    adding project

---
+++ App_Blog/templatetags/custom_filters.py
@@ -0,0 +1,10 @@
+from django import template
+
+register = template.Library()
+
+
+def range_filter(value):
+    return value[0:300] + ".................."
+
+
+register.filter('range_filter', range_filter)
